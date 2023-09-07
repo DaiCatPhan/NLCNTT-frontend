7f2bbf042c9066a0811d7e29584c9c341ef9c631
Phan Dai Cat <phandaicat12032002@gmail.com>
2023-09-07 23:43:36 +0700

    homeAdminNavbar

---
+++ src/admin/component/ItemNavBar/ItemNavBar.jsx
@@ -0,0 +1,9 @@
+import className from "classnames/bind";
+import styles from "./ItemNavBar.module.scss";
+import { Link } from "react-router-dom";
+
+const cx = className.bind(styles);
+
+ 
+
+export default ItemNavBar;
--- src/admin/component/NavBarAdmin/NavBarAdmin.jsx
+++ src/admin/component/NavBarAdmin/NavBarAdmin.jsx
@@ -1,53 +1,187 @@
 import className from "classnames/bind";
 import styles from "./NavBarAdmin.module.scss";
 import { Link } from "react-router-dom";
-
+import { useEffect, useRef, useState } from "react";
 const cx = className.bind(styles);
 
 function NavBarAdmin() {
+  const [isActiveProduct, setIsAviteProduct] = useState(false);
+  const [iconDownProduct, setIconDownProduct] = useState(false);
+  const [isActiveUser, setIsAviteUser] = useState(false);
+  const [iconDownUser, setIconDownUser] = useState(false);
+
+  const handleOnlickProduct = () => {
+    setIsAviteProduct(!isActiveProduct);
+    setIconDownProduct(!iconDownProduct);
+  };
+
+  const handleOnlickUser = () => {
+    setIsAviteUser(!isActiveUser);
+    setIconDownUser(!iconDownUser);
+  };
   return (
     <div className={cx("wrapper")}>
-      <ul className={cx("menu")}>
-        <li className={cx("item", "active")}>
-          <Link>
-            <div className={cx("item_left")}>
+      <div className={cx("menu")}>
+        <ul className={cx("listMenu")}>
+          <li className={cx("itemMenu" )}>
+            <Link>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                height="1em"
-                viewBox="0 0 576 512"
+                className="icon icon-tabler icon-tabler-home-2"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                color="white"
               >
-                <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 12l-2 0l9 -9l9 9l-2 0"></path>
+                <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                <path d="M10 12h4v4h-4z"></path>
               </svg>
-              <span>Home</span>
-            </div>
-            <div className={cx("item_right")}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="1em"
-                viewBox="0 0 320 512"
-              >
-                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-              </svg>
-            </div>
-          </Link>
-        </li>
+              Home
+            </Link>
+          </li>
 
-        <li className={cx("item")}>
-          <Link>
-            <div className={cx("item_left")}>
+          <li className={cx("itemMenu", `${isActiveProduct ? "active" : ""}`)}>
+            <Link>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                height="1em"
-                viewBox="0 0 576 512"
+                className="icon icon-tabler icon-tabler-home-2"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                color="white"
               >
-                <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 12l-2 0l9 -9l9 9l-2 0"></path>
+                <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                <path d="M10 12h4v4h-4z"></path>
               </svg>
-              <span>Home</span>
-            </div>
-            <div></div>
-          </Link>
-        </li>
-      </ul>
+              Tour
+              {!iconDownProduct ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-chevron-right"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  onClick={handleOnlickProduct}
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M9 6l6 6l-6 6"></path>
+                </svg>
+              ) : (
+                <svg
+                  onClick={handleOnlickProduct}
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                </svg>
+              )}
+            </Link>
+            <ul className={cx("ulchild")}>
+              <Link>
+                <li>List Tour</li>
+              </Link>
+              <Link>
+                <li>Add Tour</li>
+              </Link>
+            </ul>
+          </li>
+
+          <li className={cx("itemMenu", `${isActiveUser ? "active" : ""}`)}>
+            <Link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-user"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                color="white"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+              </svg>
+              User
+              {!iconDownUser ? (
+                <svg
+                  className={cx("iconRight")}
+                  onClick={handleOnlickUser}
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 320 512"
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+                </svg>
+              ) : (
+                <svg
+                  onClick={handleOnlickUser}
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 512 512"
+                >
+                  <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+                </svg>
+              )}
+            </Link>
+            <ul className={cx("ulchild")}>
+              <Link>
+                <li>List User</li>
+              </Link>
+              <Link>
+                <li>Edit product</li>
+              </Link>
+              <Link></Link>
+            </ul>
+          </li>
+
+          <li className={cx("itemMenu")}>
+            <Link>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-settings"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                color="white"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
+                <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+              </svg>
+              Setting
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
