f6bcf9830ce8c747aacc1a2aa28d4a62f28361d5
Phan Dai Cat <phandaicat12032002@gmail.com>
2023-10-09 11:11:58 +0700

    react select dynamic finish

--- src/testUI/testUI.jsx
+++ src/testUI/testUI.jsx
@@ -4,20 +4,68 @@ const cx = className.bind(styles);
 import Select from "react-select";
 import { useState } from "react";
 
+const dataArray = [
+  {
+    id: 1,
+    age: 12,
+    label: "Phan Dai Cat",
+    value: "PHan Dai Cat",
+  },
+  {
+    id: 2,
+    age: 20,
+    label: "Nguyen Van A",
+    value: "NGuyen VAn A",
+  },
+  {
+    id: 3,
+    age: 50,
+    label: "Phan Dai Cat",
+    value: "PHan Dai Cat",
+  },
+  {
+    id: 2,
+    age: 20,
+    label: "Nguyen Van B",
+    value: "NGuyen VAn B",
+  },
+  {
+    id: 2,
+    age: 20,
+    label: "Nguyen Van C",
+    value: "NGuyen VAn C",
+  },
+];
+
 function TestUi() {
   const [valueSelect, setValueSelect] = useState("");
   console.log("valueSelect", valueSelect);
 
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ];
+  // const options = [
+  //   { value: "chocolate", label: "Chocolate" },
+  //   { value: "strawberry", label: "Strawberry" },
+  //   { value: "vanilla", label: "Vanilla" },
+  // ];
 
   const handleChange = (selectedOption) => {
     console.log(">>>selectedOption", selectedOption);
     setValueSelect(selectedOption.value);
   };
+
+  const handleBuild = (inputData) => {
+    let result = [];
+    if (inputData && inputData.length > 0) {
+      inputData.map((item, index) => {
+        let object = {};
+        object.label = item.label;
+        object.value = item.value;
+        result.push(object);
+      });
+    }
+    return result;
+  };
+
+  const options = handleBuild(dataArray);
 
   return (
     <div className={cx("wrapper")}>
