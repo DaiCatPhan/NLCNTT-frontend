078a8009bc1c983d1747b275e9a2d2e6fdf169bd
Phan Dai Cat <phandaicat12032002@gmail.com>
2023-11-17 16:35:47 +0700

    module history update

--- src/resources/ProfileUser/Order/Order.jsx
+++ src/resources/ProfileUser/Order/Order.jsx
@@ -6,11 +6,13 @@ import { Table } from "antd";
 import { Tabs } from "antd";
 import { Collapse } from "antd";
 
-import BookingTourOrder from "./components/BookingTourOrder";
 import { useEffect, useMemo, useState } from "react";
 import moment from "moment";
-import BookingTourService from "../../../services/BookingTourService";
+import { IconBackspaceFilled } from "@tabler/icons-react";
+import { toast } from "react-toastify";
 
+import BookingTourOrder from "./components/BookingTourOrder";
+import BookingTourService from "../../../services/BookingTourService";
 import useAuth from "../../../hook/useAuth";
 
 function Order() {
@@ -77,20 +79,40 @@ function Order() {
     fetchData();
   }, [current, pageSize, status]);
 
+  const handleDelete = async (data) => {
+    const deleteData = {
+      idBookingTour: data.id,
+      idCustomer: data.idCustomer,
+    };
+    const res = await BookingTourService.deleteBookingTour(deleteData);
+    if (res && res.data.EC === 0) {
+      toast.success(res.data.EM);
+      fetchData();
+    } else {
+      toast.success(res.data.EM);
+    }
+  };
+
   const columns = [
     {
-      title: "Name",
-      dataIndex: "Customer",
-      key: "name",
-      render: (Customer) => <div>{Customer.name}</div>,
+      title: "Ngày đặt đơn  ",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (createdAt) => (
+        <div>{moment(createdAt).format("DD-MM-YYYY")}</div>
+      ),
     },
 
     {
       title: "Tổng tiền đơn ",
       dataIndex: "money",
       key: "money",
-      render: (money) => <div>{money.toLocaleString("vi-VN")}</div>,
+      render: (money) => {
+        const numericValue = parseFloat(money);
+        return <div>{numericValue.toLocaleString("vi-VN")} đ</div>;
+      },
     },
+
     {
       title: "Trạng thái",
       dataIndex: "status",
@@ -101,6 +123,7 @@ function Order() {
         </div>
       ),
     },
+
     {
       title: "Lịch",
       dataIndex: "Calendar",
@@ -108,18 +131,21 @@ function Order() {
       render: (Calendar) => (
         <div className={cx(" ")}>
           <div>
-            Ngày khởi hành : {moment(Calendar?.startDay).format("YYYY-MM-DD")}
+            Ngày khởi hành : {moment(Calendar?.startDay).format("DD-MM-YYYY")}
           </div>
           <div>
-            Ngày kết thúc : {moment(Calendar?.endDay).format("YYYY-MM-DD")}
+            Ngày kết thúc : {moment(Calendar?.endDay).format("DD-MM-YYYY")}
           </div>
         </div>
       ),
     },
+    
     {
       title: "Chi tiết đơn ",
       key: "dataItem",
       render: (dataItem) => {
+        const priceAdultCus = parseFloat(dataItem?.Calendar?.Tour?.priceAdult);
+        const priceChildCus = parseFloat(dataItem?.Calendar?.Tour?.priceChild);
         const itemsCollapse = [
           {
             key: "1",
@@ -142,10 +168,10 @@ function Order() {
                 <div className={cx("row")}>
                   <div className={cx("col-lg")}>
                     <div>
-                      Giá người lớn : {dataItem?.Calendar?.Tour?.priceAdult}
+                      Giá người lớn : {priceAdultCus.toLocaleString("vi-VN")} đ
                     </div>
                     <div>
-                      Giá trẻ em : {dataItem?.Calendar?.Tour?.priceChild}
+                      Giá trẻ em : {priceChildCus.toLocaleString("vi-VN")} đ
                     </div>
                   </div>
                   <div className={cx("col-lg")}>
@@ -160,11 +186,29 @@ function Order() {
         return <Collapse key={1} items={itemsCollapse}></Collapse>;
       },
     },
+
+    {
+      title: "Action",
+      key: "Action",
+      render: (record) => {
+        return (
+          status == 0 && (
+            <div>
+              <IconBackspaceFilled
+                onClick={() => handleDelete(record)}
+                style={{ color: "red" }}
+                className={cx("pointer")}
+              />
+            </div>
+          )
+        );
+      },
+    },
   ];
 
   return (
     <div className={cx("wrapper")}>
-      <div className={cx("bodyWrapper p-5")}>
+      <div className={cx("bodyWrapper")}>
         <div>
           <Tabs defaultActiveKey="0" items={items} onChange={onChangeTab} />
         </div>
